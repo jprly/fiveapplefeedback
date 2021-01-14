0742f7974e375d07a894e257c0a1fa2c8783dda8
Jon Riley <jprly@outlook.com>
2021-01-14 13:33:45 -0600

    first push

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,10 +1,10 @@
 module.exports = {
   // keep it short! shown in the header
-  title: 'Demo of the Demo',
+  title: "Demo",
 
   // these are all optional and add links to the footer
-  repo: 'learnwithjason/demo-base',
+  repo: "learnwithjason/demo-base",
   episode:
-    'https://www.learnwithjason.dev/creating-css-variable-font-text-effects',
-  tutorial: 'https://codepen.io/jlengstorf/pen/QWbdLjb',
+    "https://www.learnwithjason.dev/creating-css-variable-font-text-effects",
+  tutorial: "https://codepen.io/jlengstorf/pen/QWbdLjb",
 };
