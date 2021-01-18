f789f3b8a74031bc621fee0679a35c580e5e05c0
Jon Riley <jprly@outlook.com>
2021-01-18 15:28:37 -0600

    fetch feedback

--- functions/get-feedback.js
+++ functions/get-feedback.js
@@ -1,35 +1,21 @@
-const { faunaFetch } = require("./utils/fauna");
+import faunadb, { query as q } from "faunadb";
 
 exports.handler = async (event) => {
-  const { user } = JSON.parse(event.body);
-
-  // store the Netlify and Stripe IDs in Fauna
-  const feedbackResult = await faunaFetch({
-    query: `
-    Map(
-        Paginate(
-          Match(Index("fb_by_title"), "NVGC-L2-U4-LC1-2")
-        ),
-        Lambda(
-          "feedback",
-          Get(Var("feedback"))
-        )
-      )
-    `,
-    variables: {
-      //   netlifyID: user.id,
-      //   stripeID: customer.id,
-    },
+  var adminClient = new faunadb.Client({
+    secret: `${process.env.FAUNA_SERVER_KEY}`,
   });
 
-  console.log(feedbackResult);
+  var serverClient = new faunadb.Client({
+    secret: `${process.env.FAUNA_SERVER_KEY}`,
+  });
+
+  const feedbackMatch = serverClient
+    .query(q.Get(q.Match(q.Index("fb"), "NVGC-L2-U4-LC1-2")))
+
+    .then((ret) => console.log(ret));
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      app_metadata: {
-        feedback: feedbackResult,
-      },
-    }),
+    body: `Hello ${feedbackMatch}!`,
   };
 };
